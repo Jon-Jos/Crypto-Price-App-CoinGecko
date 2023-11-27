8309affbffa8fd26f0e40cc05cb4dd7c76e86865
Jon-Jos <jonthaiparambil@gmail.com>
2023-11-27 17:12:59 +0530

    CryptoPulse

---
+++ cryptopulse/src/stores/homeStore.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import { create } from 'zustand' // copied from  https://github.com/pmndrs/zustand
+
+const homeStore = create((set) => ({
+    coins: [],
+    query: '',
+
+    setQuery: (e) => {
+        set({ query: e.target.value })
+        homeStore.getState().searchCoins()
+    },
+
+    searchCoins: async () => {
+        const { query } = homeStore.getState();
+        const res = await axios.get(`https://api.coingecko.com/api/v3/search?query=${query}`)
+        console.log(res)
+    },
+    fetchCoins: async () => {
+        // the below is a promise so async await is used!
+        const res = await axios.get('https://api.coingecko.com/api/v3/search/trending')     // link source: https://www.coingecko.com/api/documentation
+        const coins = res.data.coins.map(coin => {
+            return {
+                name: coin.item.name,
+                image: coin.item.large,
+                id: coin.item.id,
+                priceBtc: coin.item.price_btc
+            }
+        })
+        set({ coins })
+    }
+}))
+
+export default homeStore;
